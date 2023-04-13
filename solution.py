61962aa0f6d2b650b189bbf5beb6483ea7709786
Rasulchek <rasulglash@gmail.com>
2023-04-13 22:58:46 +0300

    yeS

--- solution.py
+++ solution.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import *
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 680977959  # Ваш chat ID, не меняйте название переменной
+# data1 = pd.read_table('historical_data.csv', sep=",", header=0, index_col=0)
+# d1 = data1.to_numpy()
+# data2 = pd.read_table('modified_data_of_type_3.csv', sep=",", header=0, index_col=0)
+# d2 = data2.to_numpy()
+#
+# print(data1)
+# print(d1)
+# print(data2)
+# print(d2)
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    sta, p = ks_2samp(*x, *y)
+    return p < 0.09  # Ваш ответ, True или False
